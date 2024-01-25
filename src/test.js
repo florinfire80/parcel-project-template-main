958108d2ab69260cfca07456d3757d4256fe2074
Florin <florinfire80@yahoo.com>
2024-01-25 20:44:30 +0200

    Create test.js

---
+++ src/test.js
@@ -0,0 +1,5 @@
+const API_KEY = '07aed853a2b3116bf7e19dfeee63b968';
+const API_URL =
+  'https://api.openweathermap.org/data/2.5/weather?units=metric&q=cluj';
+const API_URL_BASE =
+  'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
